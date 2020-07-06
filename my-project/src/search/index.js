be9fd5adfd864eb9956c99111ae42a63e50e4845
guok <guok@minstone.com.cn>
2020-07-06 21:37:18 +0800

    webpack学习demo

--- my-project/src/search/index.js
+++ my-project/src/search/index.js
@@ -7,12 +7,33 @@ import '../../common/index.js';
 import { kaimo666 } from './tree-shaking.js';
 
 class Search extends React.Component {
+
+  constructor() {
+    super(...arguments);
+
+    this.state = {
+      Text: null
+    }
+  }
+
+  loadComponent() {
+    // 动态加在text.js，返回一个promise
+    import('./text.js').then((Text) => {
+      console.log(Text);
+      this.setState({
+        Text: Text.default
+      });
+    })
+  }
+
   render() {
     const kaimo = kaimo666();
+    const { Text } = this.state;
     return <div className="search-text">
       { kaimo }
       凯小默的博客666
-      <img src={ logo } />
+      { Text ? <Text /> : null }
+      <img src={ logo } onClick={ this.loadComponent.bind(this) } />
     </div>
   }
 }
